a6181c61bca832b52d6666124ed622ab2d98440c
Jeff Larson <thejefflarson@gmail.com>
2020-09-28 23:19:08 -0700

    more futzing

--- components/parser/parser.c
+++ components/parser/parser.c
@@ -1,5 +1,30 @@
+#include "battery.pb.h"
+#include "esp_err.h"
+#include <stddef.h>
+
 /*!max:re2c*/
 #ifndef YYMAXFILL
 #define YYMAXFILL 0
 #error This file needs to be preprocessed with re2c
 #endif
+
+typedef struct {
+  uint8_t *data;
+  size_t length;
+} _buffer_t;
+
+int buffer_init(_buffer_t *buf) {
+  buf->data = calloc(1 + YYMAXFILL, sizeof(uint8_t));
+  buf->length = 0;
+  if (buf->data == NULL) {
+    return ESP_ERR_NO_MEM;
+  } else {
+    return ESP_OK;
+  }
+}
+
+void buffer_free(_buffer_t *buf) { free(buf->data); }
+
+typedef enum { MORE, DONE } parser_state_t;
+// https://re2c.org/manual/manual_c.html#storable-state
+parser_state_t lex(_buffer_t *buf, battery_Stat *stat) { return DONE; }
--- components/parser/parser.h
+++ components/parser/parser.h
@@ -1,1 +1,9 @@
-
+#ifndef PARSER_H_
+#define PARSER_H_
+#include "battery.pb.h"
+typedef struct _buffer_t *buffer_t;
+typedef enum { MORE, DONE } parser_state_t;
+int buffer_init(buffer_t buf);
+void buffer_free(buffer_t buf);
+parser_state_t lex(buffer_t *buf, battery_Stat *stat);
+#endif
